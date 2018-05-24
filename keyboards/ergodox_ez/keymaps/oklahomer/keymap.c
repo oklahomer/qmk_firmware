573821d8baad1acb05d5d4f59efe03e7a82482db
Oklahomer <hagiwara.go@gmail.com>
2018-05-24 14:13:49 +0900

    Customize shift key

--- keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
+++ keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
@@ -22,45 +22,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | TAB    |   Q  |   W  |   E  |   R  |   T  |LAlt+ |           |LGui+ |   Y  |   U  |   I  |   O  |   P  | BkSp   |
  * |--------+------+------+------+------+------| Space|           | Space|------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
- * |--------+------+------+------+------+------|      |           |  `   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LALT |      |      |   \  |   '  |                                       |   -  |   =  |   [  |   ]  | RALT |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,----------------.
  *                                        |      |      |       |        |       |
- *                                 ,------|------|------|       |--------+-------+------.
- *                                 |      |      |      |       |        |       |      |
- *                                 | Esc  | LGui |------|       |--------| RGui  |Space |
- *                                 |      |      |  L1  |       | RShift |       |      |
- *                                 `--------------------'       `-----------------------'
+ *                                ,-------|------|------|       |--------+-------+------.
+ *                                |       |      |      |       |        |       |      |
+ *                                | Esc/  | LGui |------|       |--------| RGui  |Space |
+ *                                | LShift|      |  L1  |       |    `   |       |      |
+ *                                `---------------------'       `-----------------------'
  *
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,   KC_1,  KC_2,  KC_3,    KC_4,     KC_5,          TG(MDIA),
-        KC_TAB,   KC_Q,  KC_W,  KC_E,    KC_R,     KC_T,          LALT(KC_SPACE),
-        KC_LCTRL, KC_A,  KC_S,  KC_D,    KC_F,     KC_G,
-        KC_LSFT,  KC_Z,  KC_X,  KC_C,    KC_V,     KC_B,          KC_NO,
+        KC_ESC,   KC_1,  KC_2,  KC_3,    KC_4,          KC_5,    TG(MDIA),
+        KC_TAB,   KC_Q,  KC_W,  KC_E,    KC_R,          KC_T,    LALT(KC_SPACE),
+        KC_LCTRL, KC_A,  KC_S,  KC_D,    KC_F,          KC_G,
+        KC_LSFT,  KC_Z,  KC_X,  KC_C,    KC_V,          KC_B,    KC_NO,
         KC_LALT,  KC_NO, KC_NO, KC_BSLS, KC_QUOT,
-                                                   KC_NO,         KC_NO,
-                                                                  KC_NO,
-                                         KC_ESC,   KC_LGUI,       MO(SYMB),
+                                                        KC_NO,   KC_NO,
+                                                                 KC_NO,
+                                         SFT_T(KC_ESC), KC_LGUI, MO(SYMB),
         // right hand
              KC_NO,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
              LGUI(KC_SPACE), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPACE,
                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-             KC_GRV,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+             KC_NO,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                       KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_RALT,
              KC_NO,          KC_NO,
              KC_NO,
-             KC_RSFT,        KC_RGUI, KC_SPACE
+             KC_GRV,         KC_RGUI, KC_SPACE
     ),
 /* Keymap 1: Symbol Layer
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * ,--------------------------------------------------.            ,--------------------------------------------------.
  * |Version |  F1  |  F2  |  F3  |  F4   |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+-------+-------------|           |------+------+------+------+------+------+--------|
  * |LGUI+F1 |      |      |      |       |      |      |           |      |      |      |      |      |  F11 |        |
