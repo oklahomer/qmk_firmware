ecf82611e8fcb634991b80029c07f15aeb8cae92
Oklahomer <hagiwara.go@gmail.com>
2018-12-10 15:17:11 +0900

    Modify some key maps

--- keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
+++ keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Space|           | Space|------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |  JP  |           |  EN  |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LALT |      |      |      |  L2  |                                       |  L1  |      |      |      | RALT |
  *   `----------------------------------'                                       `----------------------------------'
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_1,  KC_2,   KC_3,    KC_4,     KC_5,    KC_NO,
         KC_TAB,   KC_Q,  KC_W,   KC_E,    KC_R,     KC_T,    LALT(KC_SPACE),
         KC_LCTRL, KC_A,  KC_S,   KC_D,    KC_F,     KC_G,
-        KC_LSFT,  KC_Z,  KC_X,   KC_C,    KC_V,     KC_B,    KC_LANG1,
+        KC_LSFT,  KC_Z,  KC_X,   KC_C,    KC_V,     KC_B,    KC_NO,
         KC_LALT,  KC_NO, KC_NO,  KC_NO,   MO(MDIA),
                                                     KC_NO,   KC_NO,
                                                              KC_NO,
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_NO,          KC_6,    KC_7,         KC_8,    KC_9,    KC_0,    KC_GRV,
              LGUI(KC_SPACE), KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,    KC_BSPACE,
                              KC_H,    KC_J,         KC_K,    KC_L,    KC_SCLN, KC_ENT,
-             KC_LANG2,       KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+             KC_NO,          KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                       MO(SYMB),     KC_NO,   KC_NO,   KC_NO,   KC_RALT,
              KC_NO,          KC_NO,
              KC_NO,
@@ -68,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |Version |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |LGUI+F1 |   1  |   2  |   3  |   4  |   5  |      |           |      |  6   |  7   |  8   |  9   |  0   |        |
+ * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |  6   |  7   |  8   |  9   |  0   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |  `   |   \  |   "  |      |      |           |      |      |   -  |  =   |  [   |  ]   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      | ESC  |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      | ESC  |  TAB | BkSp |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                        `---------------------------------'
@@ -87,14 +87,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       M(0),        KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
-       LGUI(KC_F1), KC_1,     KC_2,    KC_3,    KC_4,     KC_5,    KC_NO,
-       KC_TRNS,     KC_NO, KC_GRV,  KC_BSLS, KC_QUOT,  KC_NO,
-       KC_TRNS,     KC_NO,    KC_NO,   KC_ESC,  KC_NO,    KC_NO,   KC_NO,
-       KC_TRNS,     KC_NO,    KC_NO,   KC_NO,   KC_NO,
-                                                          KC_TRNS, KC_TRNS,
-                                                                   KC_TRNS,
-                                                KC_TRNS,  KC_TRNS, KC_TRNS,
+       M(0),    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,
+       KC_TRNS, KC_1,     KC_2,    KC_3,    KC_4,    KC_5,      KC_NO,
+       KC_TRNS, KC_NO,    KC_GRV,  KC_BSLS, KC_QUOT, KC_NO,
+       KC_TRNS, KC_NO,    KC_NO,   KC_ESC,  KC_TAB,  KC_BSPACE, KC_NO,
+       KC_TRNS, KC_NO,    KC_NO,   KC_NO,   KC_NO,
+                                                     KC_TRNS,   KC_TRNS,
+                                                                KC_TRNS,
+                                            KC_TRNS, KC_TRNS,   KC_TRNS,
        // right hand
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
        KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
