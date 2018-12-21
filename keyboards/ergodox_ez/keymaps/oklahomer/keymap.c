e3b62658e4612d5e549f1430413453b2ad37606c
Oklahomer <hagiwara.go@gmail.com>
2018-12-21 11:32:56 +0900

    Accept comma and period inupt on second layer

--- keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
+++ keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        | TAB  |  `   |   \  |   "  | BkSp |      |           |      |      |   -  |  =   |  [   |  ]   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      | ESC  |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      | ESC  |      |      |      |           |      |      |      |  ,   |  .   |  /   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                        `---------------------------------'
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
        KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
                 KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_TRNS,
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+       KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
