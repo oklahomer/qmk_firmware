cd0aaa394d4b9aada147273f28be92b404c84c9d
Oklahomer <hagiwara.go@gmail.com>
2018-12-21 12:26:29 +0900

    Remove obsolete keymap

--- keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
+++ keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   `    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |LAlt+ |           |LGui+ |   Y  |   U  |   I  |   O  |   P  | BkSp   |
- * |--------+------+------+------+------+------| Space|           | Space|------+------+------+------+------+--------|
+ * | TAB    |   Q  |   W  |   E  |   R  |   T  |LAlt+ |           |      |   Y  |   U  |   I  |   O  |   P  | BkSp   |
+ * |--------+------+------+------+------+------| Space|           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
@@ -54,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                              KC_NO,
                                           KC_RSFT,  KC_LGUI, KC_NO,
         // right hand
-             KC_NO,          KC_6,    KC_7,         KC_8,    KC_9,    KC_0,    KC_GRV,
-             LGUI(KC_SPACE), KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,    KC_BSPACE,
-                             KC_H,    KC_J,         KC_K,    KC_L,    KC_SCLN, KC_ENT,
-             KC_NO,          KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                      MO(SYMB),     KC_NO,   KC_NO,   KC_NO,   KC_RALT,
-             KC_NO,          KC_NO,
+             KC_NO, KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_GRV,
+             KC_NO, KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPACE,
+                    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
+             KC_NO, KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                             MO(SYMB), KC_NO,   KC_NO,   KC_NO,   KC_RALT,
+             KC_NO, KC_NO,
              KC_NO,
-             KC_NO,          KC_RGUI, KC_SPACE
+             KC_NO, KC_RGUI, KC_SPACE
     ),
 /* Keymap 1: Symbol Layer
  *
