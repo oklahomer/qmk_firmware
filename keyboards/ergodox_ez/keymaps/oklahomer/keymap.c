0ce8648ea71c6c606053e9be9b4170572a3a4a7e
Oklahomer <hagiwara.go@gmail.com>
2023-01-12 21:14:23 +0900

    Update the layer switching keys

--- keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
+++ keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|Ctrl+1|           |Ctrl+2|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LALT |      |      |      |  L2  |                                       |  L1  |      |      |      | RALT |
+ *   | LALT |      |      |  L2  |  L1  |                                       |  L1  |  L2  |      |      | RALT |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,----------------.
  *                                        |      |      |       |        |       |
@@ -49,20 +49,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,   KC_1,  KC_2,   KC_3,    KC_4,     KC_5,    KC_NO,
-        KC_TAB,   KC_Q,  KC_W,   KC_E,    KC_R,     KC_T,    LALT(KC_SPACE),
-        KC_LCTRL, KC_A,  KC_S,   KC_D,    KC_F,     KC_G,
-        KC_LSFT,  KC_Z,  KC_X,   KC_C,    KC_V,     KC_B,    LCTL(KC_1),
-        KC_LALT,  KC_NO, KC_NO,  KC_NO,   MO(MDIA),
-                                                    KC_NO,   KC_NO,
-                                                             KC_NO,
-                                          KC_ESC,  KC_LGUI, KC_NO,
+        KC_ESC,   KC_1,  KC_2,   KC_3,     KC_4,     KC_5,    KC_NO,
+        KC_TAB,   KC_Q,  KC_W,   KC_E,     KC_R,     KC_T,    LALT(KC_SPACE),
+        KC_LCTRL, KC_A,  KC_S,   KC_D,     KC_F,     KC_G,
+        KC_LSFT,  KC_Z,  KC_X,   KC_C,     KC_V,     KC_B,    LCTL(KC_1),
+        KC_LALT,  KC_NO, KC_NO,  MO(MDIA), MO(SYMB),
+                                                     KC_NO,   KC_NO,
+                                                              KC_NO,
+                                           KC_ESC,   KC_LGUI, KC_NO,
         // right hand
-             SLEEP,      KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_NO,
-             OSL(APP),   KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_BSPACE,
-                         KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_ENT,
-             LCTL(KC_2), KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                                  MO(SYMB), KC_NO,   KC_NO,  KC_NO,   KC_RALT,
+             SLEEP,      KC_6,    KC_7,     KC_8,     KC_9,   KC_0,    KC_NO,
+             OSL(APP),   KC_Y,    KC_U,     KC_I,     KC_O,   KC_P,    KC_BSPACE,
+                         KC_H,    KC_J,     KC_K,     KC_L,   KC_SCLN, KC_ENT,
+             LCTL(KC_2), KC_N,    KC_M,     KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+                                  MO(SYMB), MO(MDIA), KC_NO,  KC_NO,   KC_RALT,
              KC_NO,      KC_NO,
              KC_NO,
              KC_NO,      KC_RGUI, KC_SPACE
