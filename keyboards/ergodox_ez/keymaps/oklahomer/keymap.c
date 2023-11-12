163a00ab08ff74d30fae17210f4de0d6b4932370
Oklahomer <hagiwara.go@gmail.com>
2023-11-12 12:11:43 +0900

    Update boot setting

--- keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
+++ keyboards/ergodox_ez/keymaps/oklahomer/keymap.c
@@ -132,14 +132,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 [MDIA] = LAYOUT_ergodox(
-       KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,
-       KC_NO,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_NO,
-       KC_NO,     KC_VOLD, KC_VOLU, KC_MUTE, KC_NO,   KC_BSPC,
-       KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,
-       QK_REBOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                                      KC_TRNS,   KC_TRNS,
-                                                                 KC_TRNS,
-                                             KC_TRNS, KC_TRNS,   KC_TRNS,
+       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,
+       KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_NO,
+       KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE, KC_NO,   KC_BSPC,
+       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,
+       QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                                    KC_TRNS,   KC_TRNS,
+                                                               KC_TRNS,
+                                           KC_TRNS, KC_TRNS,   KC_TRNS,
     // right hand
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
        KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_NO,
